cbabd9f561c0e281fabf13e0a90d15c182857fc0
Vlad Fratila <101815144+Fratzi1@users.noreply.github.com>
2025-01-17 23:27:01 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = "nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_user = "snlf1e75emr6xu8z";
+$db_pass = "xaysrwe724xapov3";
+$db_name = "ze4xiva4lm9mohzr";
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("database connection error");
+
+?>
